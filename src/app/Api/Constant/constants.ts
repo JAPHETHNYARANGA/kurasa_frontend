12f82df7e5612706520711dba7fea8038b664d3b
JAPHETHNYARANGA <nyaranga4@gmail.com>
2023-03-02 11:52:05 +0300

    Corrected spelling error on live link

--- src/app/Api/Constant/constants.ts
+++ src/app/Api/Constant/constants.ts
@@ -3,5 +3,5 @@ export class Constants {
     // static readonly BASE_URL = 'http://127.0.0.1:8000/api/'
 
     // remote
-    static readonly BASE_URL = 'kurasa.lakecitystudios.com/api/'
+    static readonly BASE_URL = 'kurasa.lakecitystudios.co.ke/api/'
 }
